79fbd2f8d2c37ae79c08a79cac69b73df2c879d7
LudivinDivin <augustcasse@gmail.com>
2025-03-13 15:02:48 +0100

    Layout Transition Multiple Items

--- app/index.tsx
+++ app/index.tsx
@@ -1,42 +1,54 @@
-import { useState } from "react";
-import { Button, StyleSheet, View } from "react-native";
-import Animated, {
-  FadeIn,
-  FadeOut,
-  LinearTransition
-} from "react-native-reanimated";
+import React, { useState } from 'react';
+import { View, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
+import Animated, { FadeOut, JumpingTransition } from 'react-native-reanimated';
 
-export default function Index() {
+type TItem = {
+  id: number;
+  emoji: string;
+  color: string;
+}
 
-  const [expanded, setExpanded] = useState(false)
-  const [visible, setVisible] = useState(false)
+const INITIAL_LIST = [
+  { id: 1, emoji: '🍌', color: '#b58df1' },
+  { id: 2, emoji: '🍎', color: '#ffe780' },
+  { id: 3, emoji: '🥛', color: '#fa7f7c' },
+  { id: 4, emoji: '🍙', color: '#82cab2' },
+  { id: 5, emoji: '🍇', color: '#fa7f7c' },
+  { id: 6, emoji: '🍕', color: '#b58df1' },
+  { id: 7, emoji: '🍔', color: '#ffe780' },
+  { id: 8, emoji: '🍟', color: '#b58df1' },
+  { id: 9, emoji: '🍩', color: '#82cab2' },
+] as TItem[];
+
+export default function App() {
+  const [items, setItems] = useState(INITIAL_LIST);
+
+  const removeItem = (idToRemove: number) => {
+    const updatedItems = items.filter((item) => item.id !== idToRemove);
+    setItems(updatedItems);
+  };
 
   return (
-    <View
-      style={styles.container}
-    >
-      {visible &&
-        <Animated.View
-          layout={LinearTransition.springify()}
-          exiting={FadeOut.duration(500)}
-          entering={FadeIn.duration(500)}
-          style={[
-            styles.square,
-            { height: expanded ? 200 : 100 }
-          ]}
-        />
-      }
-      <Button
-        title="Press me"
-        onPress={() => setExpanded(!expanded)}
-      />
-      <Button
-        title="Change Visilbility"
-        onPress={() => setVisible(!visible)}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.gridContainer}>
+        {items.map((item) => (
+          <Animated.View
+            //key est nécessaire pour un que layout applique correctemment l'animation
+            key={item.id}
+            layout={JumpingTransition} //on applique le Layout ici
+            exiting={FadeOut}
+            style={[styles.tileContainer, { backgroundColor: item.color }]}>
+            <TouchableOpacity onPress={() => removeItem(item.id)} style={styles.tile}>
+              <Animated.Text style={styles.tileLabel}>{item.emoji}</Animated.Text>
+            </TouchableOpacity>
+          </Animated.View>
+        ))}
+      </View>
+    </SafeAreaView>
   );
 }
+
+
 
 
 
@@ -46,25 +58,35 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 32,
+    width: 'auto',
+    display: 'flex',
+    minHeight: 300,
   },
-  square: {
-    width: 100,
-    height: 100,
-    backgroundColor: "lightblue",
-    borderRadius: 12,
-  }
+  gridContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingHorizontal: 32,
+  },
+  tileContainer: {
+    width: '20%',
+    margin: '1%',
+    borderRadius: 16,
+    minHeight: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tile: {
+    flex: 1,
+    height: '100%',
+    width: ' 100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tileLabel: {
+    color: '#f8f9ff',
+    fontSize: 24,
+  },
 });
-
-
-
-
-
-
-
-
-
-
-
